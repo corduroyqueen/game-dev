ac3e49293a6ddacbd98c00c6d76807a6abb9dfad
Unknown <ew1152@nyu.edu>
2017-04-12 09:12:31 -0400

    post-game swap

---
+++ Game Dev/Assets/scripts/mouseIconScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mouseIconScript : MonoBehaviour {
+
+	Image imageComponent;
+	public GameObject bagSize;
+	private bagSizeScript bagSizeScript;
+
+	public Sprite dashIcon;
+	public Sprite noIcon;
+	public Sprite smashIcon;
+
+	// Use this for initialization
+	void Start () {
+		bagSize = GameObject.Find ("bagSize");
+		bagSizeScript = (bagSizeScript)bagSize.GetComponent (typeof(bagSizeScript));
+		imageComponent = gameObject.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bagSizeScript.size <= 2f) {
+			imageComponent.sprite = dashIcon;
+		} else if (bagSizeScript.size > 2f && bagSizeScript.size < 6f) {
+			imageComponent.sprite = noIcon;
+		} else if (bagSizeScript.size >= 6f) {
+			imageComponent.sprite = smashIcon;
+		}
+	}
+}
---
+++ Game Dev/Assets/scripts/treasureCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class treasureCounter : MonoBehaviour {
+
+	Text text;
+	public GameObject bagSize;
+	private bagSizeScript bagSizeScript;
+
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text> ();
+		bagSize = GameObject.Find ("bagSize");
+		bagSizeScript = (bagSizeScript)bagSize.GetComponent (typeof(bagSizeScript));
+		text.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = bagSizeScript.size.ToString();
+	}
+}
